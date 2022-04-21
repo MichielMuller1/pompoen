0d9c365d3930beeee36bb4911d8e21d7e107a2ee
HVerschueren <r0842391@student.thomasmore.be>
2022-04-21 18:04:57 +0200

    website treshold alle waardes in php

--- website/threshold.php
+++ website/threshold.php
@@ -61,7 +61,8 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['user_email'])) {
                             <input type="number" name="ventilator1" id="ventilator1" class="col-9">
 
                             <label class="switch col-3 d-flex align-items-center">
-                                <input type="checkbox" id="ventilator1Automatic">
+                                <input type="checkbox" id="ventilator1Automatic" name="ventilator1Automatic" value="0">
+                                <input type="checkbox" id="ventilator1_onOff" name="ventilator1_onOff" value="0" class="onOff">
                             </label>
                         </div>
 
@@ -70,7 +71,8 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['user_email'])) {
                         <label for="ventilator2">ventilator 2</label>
                         <input type="number" name="ventilator2" id="ventilator2">
                         <label class="switch">
-                            <input type="checkbox" id="ventilator2Automatic">
+                            <input type="checkbox" id="ventilator2Automatic" name="ventilator2Automatic" value="0">
+                            <input type="checkbox" id="ventilator2_onOff" name="ventilator1_onOff" value="0" class="onOff">
                         </label>
                     </div>
                 </div>
@@ -79,77 +81,92 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['user_email'])) {
                     <label for="raam1">raam 1</label>
                     <input type="number" name="raam1" id="raam1">
                     <label class="switch">
-                        <input type="checkbox" id="raam1Automatic">
+                        <input type="checkbox" id="raam1Automatic" name="raam1Automatic" value="0">
+                        <input type="checkbox" id="raam1_onOff" name="raam1_onOff" value="0" class="onOff">
                     </label>
                 </div>
                 <div class="form-group">
                     <label for="raam2">raam 2</label>
                     <input type="number" name="raam2" id="raam2">
                     <label class="switch">
-                        <input type="checkbox" id="raam2Automatic">
+                        <input type="checkbox" id="raam2Automatic" name="raam2Automatic" value="0">
+                        <input type="checkbox" id="raam2_onOff" name="raam2_onOff" value="0" class="onOff">
                     </label>
                 </div>
                 <div class="form-group">
                     <label for="deur1">deur 1</label>
                     <input type="number" name="deur1" id="deur1">
                     <label class="switch">
-                        <input type="checkbox" id="deur1Automatic">
+                        <input type="checkbox" id="deur1Automatic" name="deur1Automatic" value="0">
+                        <input type="checkbox" id="deur1_onOff" name="deur1_onOff" value="0" class="onOff">
                     </label>
                 </div>
                 <div class="form-group">
                     <label for="deur2"> deur 2</label>
                     <input type="number" name="deur2" id="deur2">
                     <label class="switch">
-                        <input type="checkbox" id="deur2Automatic">
+                        <input type="checkbox" id="deur2Automatic" name="deur2Automatic" value="0">
+                        <input type="checkbox" id="deur2_onOff" name="deur2_onOff" value="0" class="onOff">
                     </label>
                 </div>
                 <div class="form-group">
                     <label for="vat1MIN">vat 1 min</label>
                     <input type="number" name="vat1MIN" id="vat1MIN">
-                    <label class="switch">
-                        <input type="checkbox" id="vat1MINAutomatic">
-                    </label>
-                </div>
-                <div class="form-group">
                     <label for="vat1MAX">vat 1 max</label>
                     <input type="number" name="vat1MAX" id="vat1MAX">
                     <label class="switch">
-                        <input type="checkbox" id="vat1MAXAutomatic">
+                        <input type="checkbox" id="vat1Automatic" name="vat1Automatic" value="0">
+                        <input type="checkbox" id="vat1_bijvullen_onOff" name="vat1_bijvullen_onOff" value="0" class="onOff">
+                        <input type="checkbox" id="vat1_watergeven_onOff" name="vat1_watergeven_onOff" value="0" class="onOff">
                     </label>
                 </div>
                 <div class="form-group">
                     <label for="vat2MIN">vat 2 min</label>
                     <input type="number" name="vat2MIN" id="vat2MIN">
-                    <label class="switch">
-                        <input type="checkbox" id="vat2MINAutomatic">
-                    </label>
-                </div>
-                <div class="form-group">
                     <label for="vat2MAX">vat 2 max</label>
                     <input type="number" name="vat2MAX" id="vat2MAX">
                     <label class="switch">
-                        <input type="checkbox" id="vat2Automatic">
+                        <input type="checkbox" id="vat2Automatic" name="vat2Automatic" value="0">
+                        <input type="checkbox" id="vat2_bijvullen_onOff" name="vat2_bijvullen_onOff" value="0" class="onOff">
+                        <input type="checkbox" id="vat2_watergeven_onOff" name="vat2_watergeven_onOff" value="0" class="onOff">
+                    </label>
+                </div>
+
+                <div class="form-group">
+                    <label for="vat3MIN">vat 3 min</label>
+                    <input type="number" name="vat3MIN" id="vat3MIN">
+                    <label for="vat3MAX">vat 3 max</label>
+                    <input type="number" name="vat3MAX" id="vat3MAX">
+                    <label class="switch">
+                        <input type="checkbox" id="vat3Automatic" name="vat3Automatic" value="0">
+                        <input type="checkbox" id="vat3_bijvullen_onOff" name="vat3_bijvullen_onOff" value="0" class="onOff">
+                        <input type="checkbox" id="vat3_watergeven_onOff" name="vat3_watergeven_onOff" value="0" class="onOff">
                     </label>
                 </div>
                 <div class="form-group">
-                    <label for="grondvochtigheid1">grondvochtigheid 1</label>
-                    <input type="number" name="grondvochtigheid1" id="grondvochtigheid1">
-                    <label class="switch">
-                        <input type="checkbox" id="grondvochtigheid1Automatic">
-                    </label>
+                    <label for="grondvochtigheid1Laag1">grondvochtigheid 1 laag 1</label>
+                    <input type="number" name="grondvochtigheid1Laag1" id="grondvochtigheid1Laag1">
                 </div>
                 <div class="form-group">
-                    <label for="grondvochtigheid2">grondvochtigheid 2</label>
-                    <input type="number" name="grondvochtigheid2" id="grondvochtigheid2">
-                    <label class="switch">
-                        <input type="checkbox" id="grondvochtigheid2Automatic">
-                    </label>
+                    <label for="grondvochtigheid1Laag2">grondvochtigheid 1 laag 2</label>
+                    <input type="number" name="grondvochtigheid1Laag2" id="grondvochtigheid1Laag2">
+                </div>
+                <div class="form-group">
+                    <label for="grondvochtigheid2Laag1">grondvochtigheid 2 laag 1</label>
+                    <input type="number" name="grondvochtigheid2Laag1" id="grondvochtigheid2Laag1">
+                </div>
+                <div class="form-group">
+                    <label for="grondvochtigheid2Laag2">grondvochtigheid 2 laag 2</label>
+                    <input type="number" name="grondvochtigheid2Laag2" id="grondvochtigheid2Laag2">
                 </div>
                 <div class="form-group">
                     <label for="licht">licht</label>
                     <input type="number" name="licht" id="licht">
+                    <label for="lichtKleur">licht kleur</label>
+                    <input type="number" name="lichtKleur" id="lichtKleur">
                     <label class="switch">
-                        <input type="checkbox" id="lichtAutomatic">
+                        <input type="checkbox" id="lichtAutomatic" name="lichtAutomatic" value="0">
+                        <input type="checkbox" id="licht_onOff" name="licht_onOff" value="0" class="onOff">
                     </label>
                 </div>
                 <button type="submit" class="btn btn-primary">Kiezen</button>
--- website/todatabase.php
+++ website/todatabase.php
@@ -3,40 +3,77 @@
 session_start();
 $ventilator1 = $_POST["ventilator1"];
 echo $ventilator1 . "\n";
+$ventilator1Automatic = isset($_POST['ventilator1Automatic']) ? 1 : 0;
+echo $ventilator1Automatic . "\n";
+$ventilator1ONOFF = isset($_POST["ventilator1_onOff"])? 1 : 0;
+echo $ventilator1ONOFF . "\n";
+
 
 $ventilator2 = $_POST["ventilator2"];
+$ventilator2Automatic = isset($_POST["ventilator2Automatic"]) ? 1 : 0;
+$ventilator2ONOFF = isset($_POST["ventilator2_onOff"]) ? 1 : 0;
 echo $ventilator2 . "\n";
 
 $raam1 = $_POST["raam1"];
+$raam1Automatic = isset($_POST["raam1Automatic"]) ? 1 : 0;
+$raam1ONOFF = isset($_POST["raam1_onOff"]) ? 1 : 0;
 echo $raam1 . "\n";
 
 $raam2 = $_POST["raam2"];
+$raam2Automatic = isset($_POST["raam2Automatic"]) ? 1 : 0;
+$raam2ONOFF = isset($_POST["raam2_onOff"]) ? 1 : 0;
 echo $raam2 . "\n";
 
 $deur1 = $_POST["deur1"];
+$deur1Automatic = isset($_POST["deur1Automatic"]) ? 1 : 0;
+$deur1ONOFF = isset($_POST["deur1_onOff"]) ? 1 : 0;
 echo $deur1 . "\n";
+
 $deur2 = $_POST["deur2"];
+$deur2Automatic = isset($_POST["deur2Automatic"]) ? 1 : 0;
+$deur2ONOFF = isset($_POST["deur2_onOff"]) ? 1 : 0;
 echo $deur2 . "\n";
 
 $vat1MIN = $_POST["vat1MIN"];
-echo $vat1MIN . "\n";
-
 $vat1MAX = $_POST["vat1MAX"];
+$vat1Automatic = isset($_POST["vat1Automatic"]) ? 1 : 0;
+$vat1_bijvullenONOFF = isset($_POST["vat1_bijvullen_onOff"]) ? 1 : 0;
+$vat1_watergevenONOFF = isset($_POST["vat1_watergeven_onOff"]) ? 1 : 0;
+echo $vat1MIN . "\n";
 echo $vat1MAX . "\n";
 
 $vat2MIN = $_POST["vat2MIN"];
-echo $vat2MIN . "\n";
-
 $vat2MAX = $_POST["vat2MAX"];
+$vat2Automatic = isset($_POST["vat2Automatic"]) ? 1 : 0;
+$vat2_bijvullenONOFF = isset($_POST["vat2_bijvullen_onOff"]) ? 1 : 0;
+$vat2_watergevenONOFF = isset($_POST["vat2_watergeven_onOff"]) ? 1 : 0;
+echo $vat2MIN . "\n";
 echo $vat2MAX . "\n";
 
-$grondvochtigheid1 = $_POST["grondvochtigheid1"];
-echo $grondvochtigheid1 . "\n";
+$vat3MIN = $_POST["vat3MIN"];
+$vat3MAX = $_POST["vat3MAX"];
+$vat3Automatic = isset($_POST["vat3Automatic"]) ? 1 : 0;
+$vat3_bijvullenONOFF = isset($_POST["vat3_bijvullen_onOff"]) ? 1 : 0;
+$vat3_watergevenONOFF = isset($_POST["vat3_watergeven_onOff"]) ? 1 : 0;
+echo $vat3MIN . "\n";
+echo $vat3MAX . "\n";
 
-$grondvochtigheid2 = $_POST["grondvochtigheid2"];
-echo $grondvochtigheid2 . "\n";
+$grondvochtigheid1Laag1 = $_POST["grondvochtigheid1Laag1"];
+echo $grondvochtigheid1Laag1 . "\n";
+
+$grondvochtigheid1Laag2 = $_POST["grondvochtigheid1Laag2"];
+echo $grondvochtigheid1Laag2 . "\n";
+
+$grondvochtigheid2Laag1 = $_POST["grondvochtigheid2Laag1"];
+echo $grondvochtigheid2Laag1 . "\n";
+
+$grondvochtigheid2Laag2 = $_POST["grondvochtigheid2Laag2"];
+echo $grondvochtigheid2Laag2 . "\n";
 
 $licht = $_POST["licht"];
+$lichtKleur = $_POST["lichtKleur"];
+$lichtAutomatic = isset($_POST["lichtAutomatic"]) ? 1 : 0;
+$lichtONOFF = isset($_POST["licht_onOff"]) ? 1 : 0;
 echo $licht . "\n";
 
 $sName = "192.168.56.5";
@@ -49,14 +86,6 @@ try {
     $conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-//    $sql = "DELETE FROM threshold WHERE id=1";
-//    $conn->exec($sql);
-//
-//
-//    $sql = "INSERT INTO threshold (ventilator1, ventilator2) VALUES ($ventilator1, $ventilator2)";
-//    $conn->exec($sql);
-
-
     $stmt = $conn->prepare("select * from threshold where id=1");
     $stmt->execute();
     $result = $stmt->fetchAll();
@@ -64,18 +93,19 @@ try {
     if (!empty($result)){//only delete when there is something in the database
         $sql = "DELETE FROM threshold WHERE id=1";
         $conn->exec($sql);
+        $sql2 = "DELETE FROM automatisch WHERE id=1";
+        $conn->exec(($sql2));
     }
 
     $tijd = date('Y-m-d H:i:s');
     echo $tijd;
 
-
-    //'2022-02-01 15:30:15'
-
     //insert all the values into the database
-    $sql = "INSERT INTO `threshold` (`ID`, `tijd`, `temp ventilator 1`, `temp ventilator 2`, `temp raam 1`, `temp raam 2`, `temp deur 1`, `temp deur 2`, `min vat 1`, `max vat 1`, `min vat 2`, `max vat 2`, `grondvochtigheid 1`, `grondvochtigheid 2`, `licht`) VALUES (1,'$tijd',$ventilator1, $ventilator2,$raam1,$raam2,$deur1,$deur2,$vat1MIN,$vat1MAX,$vat2MIN,$vat2MAX,$grondvochtigheid1,$grondvochtigheid2,$licht)";
+    $sql = "INSERT INTO `threshold` (`ID`, `tijd`, `temp ventilator 1`, `temp ventilator 2`, `temp raam 1`, `temp raam 2`, `temp deur 1`, `temp deur 2`, `min vat 1`, `max vat 1`, `min vat 2`, `max vat 2`, `min vat 3`, `max vat 3`, `grondvochtigheid 1 laag 1`,`grondvochtigheid 1 laag 2`, `grondvochtigheid 2 laag 1`,`grondvochtigheid 2 laag 2`, `licht`, `lichtkleur`) VALUES (1,'$tijd',$ventilator1, $ventilator2,$raam1,$raam2,$deur1,$deur2,$vat1MIN,$vat1MAX,$vat2MIN,$vat2MAX,$vat3MIN,$vat3MAX,$grondvochtigheid1Laag1,$grondvochtigheid1Laag2,$grondvochtigheid2Laag1,$grondvochtigheid2Laag2,$licht,$lichtKleur)";
     $conn->exec($sql);
 
+    $sql2 = "INSERT INTO `automatisch` (`ID`, `tijd`,`ventilator1`,`ventilator2`,`raam 1`,`raam 2`,`deur 1`,`deur 2`,`vat 1`,`vat 2`,`vat 3`,`licht`) VALUES (1,'$tijd',$ventilator1Automatic,$ventilator2Automatic,$raam1Automatic,$raam2Automatic,$deur1Automatic,$deur2Automatic,$vat1Automatic,$vat2Automatic,$vat3Automatic,$lichtAutomatic)";
+    $conn->exec($sql2);
 
     //$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
