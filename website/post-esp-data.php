037a2a38a4e54ef69305ebae9600749757057e50
Michiel Muller <71765474+MichielMuller1@users.noreply.github.com>
2022-04-21 15:21:06 +0200

    Update post-esp-data.php

--- website/post-esp-data.php
+++ website/post-esp-data.php
@@ -31,7 +31,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             die("Connection failed: " . $conn->connect_error);
         } 
         
+        $sql = "INSERT INTO pompoen 1 (temperatuur, grondvochtigheid)
+        VALUES ('""', '"2022-04-21 15:20:38"', '" . &temperatuur . "', '" . $vochtigheid . "')";
         
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } 
+        else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     
         $conn->close();
     }
