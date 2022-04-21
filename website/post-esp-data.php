ad5a9653389d8d825feacbeebd33fca94526fd92
Michiel Muller <71765474+MichielMuller1@users.noreply.github.com>
2022-04-21 15:16:56 +0200

    Update post-esp-data.php

--- website/post-esp-data.php
+++ website/post-esp-data.php
@@ -20,28 +20,7 @@ $api_key= $temperatuur = $vochtigheid = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $api_key = test_input($_POST["api_key"]);
     if($api_key == $api_key_value) {
-        $temperatuur = test_input($_POST["temperatuur"]);
-        $grondvochtigheid = test_input($_POST["vochtigheid"]);
-
         
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
-        
-        $sql = "INSERT INTO pompoen 1 (temperatuur, grondvochtigheid)
-        VALUES ('" . &temperatuur . "', '" . $vochtigheid . "')";
-        
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-        } 
-        else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-    
-        $conn->close();
     }
     else {
         echo "Wrong API Key provided.";
