a64c8b8144e9d75f9fbb05980752a082662a9afa
Michiel Muller <71765474+MichielMuller1@users.noreply.github.com>
2022-04-21 15:14:07 +0200

    Update post-esp-data.php

--- website/post-esp-data.php
+++ website/post-esp-data.php
@@ -15,39 +15,10 @@ $password = "raspberry";
 // If you change this value, the ESP32 sketch needs to match
 $api_key_value = "tPmAT5Ab3j7F9";
 
-
+$api_key= $temperatuur = $vochtigheid = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $api_key = test_input($_POST["api_key"]);
-    if($api_key == $api_key_value) {
-        $tijd = test_input($_POST["location"]);
-        $temperatuur = test_input($_POST["temperatuur"]);
-        $grondvochtigheid = test_input($_POST["vochtigheid"]);
-
-        
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
-        
-        $sql = "INSERT INTO 'pompoen 1' (temperatuur, grondvochtigheid)
-        VALUES ('" . &temperatuur . "', '" . $vochtigheid . "')";
-        
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-        } 
-        else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-    
-        $conn->close();
-    }
-    else {
-        echo "Wrong API Key provided.";
-    }
-
+   
 }
 else {
     echo "No data posted with HTTP POST.";
