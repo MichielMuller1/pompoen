c919a2d55aa402b671ded5ba56fb718c81f524e5
Michiel Muller <71765474+MichielMuller1@users.noreply.github.com>
2022-05-05 13:51:06 +0200

    Update post-esp-data.php

--- website/post-esp-data.php
+++ website/post-esp-data.php
@@ -17,7 +17,7 @@ $api_key_value = "tPmAT5Ab3j7F9";
 
 $api_key= $temp = $hum = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $api_key = test_input($_POST["api_key"]);
     if($api_key == $api_key_value) {
         $temp = test_input($_POST["temp"]);
@@ -46,10 +46,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Wrong API Key provided.";
     }
 
-}
-else {
-    echo "No data posted with HTTP POST.";
-}
+// }
+// else {
+//     echo "No data posted with HTTP POST.";
+// }
 
 function test_input($data) {
     $data = trim($data);
