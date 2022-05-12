40ed3e68c12cb5f908b594ab27fb22e40aceb5e1
pieterjanssen <r0755997@student.thomasmore.be>
2022-05-12 15:07:25 +0200

    e

--- website/esp-data.php
+++ website/esp-data.php
@@ -35,7 +35,7 @@ if ($result = $conn->query($sql)) {
         // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
         //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
       
-		$data = array('ID' => $row_ID, 'tijd' => $row_tijd, 'temperatuur' => $row_temperatuur, 'grondvochtigheidslaag1' => $row_grondvochtigheidslaag1, 'grondvochtigheidslaag2' => $row_grondvochtigheidslaag1);
+		$data = array('pompoen1' => array('ID' => $row_ID, 'tijd' => $row_tijd, 'temperatuur' => $row_temperatuur, 'grondvochtigheidslaag1' => $row_grondvochtigheidslaag1, 'grondvochtigheidslaag2' => $row_grondvochtigheidslaag1);
 		header('Content-type: text/javascript');
 		echo json_encode($data);
     }
