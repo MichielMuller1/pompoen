98881a4bdda760a0c732dd60dc27938ff2537afb
pieterjanssen <r0755997@student.thomasmore.be>
2022-05-12 13:13:11 +0200

    leuk

---
+++ website/esp-data.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html><body>
+<?php
+
+
+$servername = "localhost";
+
+// REPLACE with your Database name
+$dbname = "pompoen";
+// REPLACE with Database user
+$username = "pi";
+// REPLACE with Database user password
+$password = "raspberry";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT ID, tijd, temperatuur, grondvochtigheidslaag1, grondvochtigheidslaag1 FROM pompoen1";
+
+echo '<table cellspacing="5" cellpadding="5">
+      <tr> 
+        <td>ID</td> 
+        <td>tijd</td> 
+        <td>temperatuur</td> 
+        <td>grondvochtigheidslaag1</td> 
+        <td>grondvochtigheidslaag2</td>
+
+      </tr>';
+ 
+if ($result = $conn->query($sql)) {
+    while ($row = $result->fetch_assoc()) {
+        $row_ID = $row["ID"];
+        $row_tijd = $row["tijd"];
+        $row_temperatuur = $row["temperatuur"];
+        $row_grondvochtigheidslaag1 = $row["grondvochtigheidslaag1"];
+        $row_grondvochtigheidslaag2 = $row["grondvochtigheidslaag2"]; 
+
+        // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
+        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
+      
+        // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
+        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
+      
+        echo '<tr> 
+                <td>' . $row_ID . '</td> 
+                <td>' . $row_tijd . '</td> 
+                <td>' . $row_temperatuur . '</td> 
+                <td>' . $row_grondvochtigheidslaag1 . '</td> 
+                <td>' . $row_grondvochtigheidslaag2 . '</td>
+              </tr>';
+    }
+    $result->free();
+}
+
+$conn->close();
+?> 
+</table>
+</body>
+</html>
