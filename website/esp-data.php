821d3bd0dbac166ca5dda9acfa8de5e8c22a1faa
pieterjanssen <r0755997@student.thomasmore.be>
2022-05-12 13:53:50 +0200

    nice

--- website/esp-data.php
+++ website/esp-data.php
@@ -31,6 +31,11 @@ echo '<table cellspacing="5" cellpadding="5">
 
       </tr>';
  
+ 
+$data = array('ID' => 'apple', 'b' => 'banana', 'c' => 'catnip');
+header('Content-type: text/javascript');
+echo json_encode($data);
+ 
 if ($result = $conn->query($sql)) {
     while ($row = $result->fetch_assoc()) {
         $row_ID = $row["ID"];
@@ -45,13 +50,9 @@ if ($result = $conn->query($sql)) {
         // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
         //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
       
-        echo '<tr> 
-                <td>' . $row_ID . '</td> 
-                <td>' . $row_tijd . '</td> 
-                <td>' . $row_temperatuur . '</td> 
-                <td>' . $row_grondvochtigheidslaag1 . '</td> 
-                <td>' . $row_grondvochtigheidslaag2 . '</td>
-              </tr>';
+		$data = array('ID' => $row_ID, 'tijd' => $row_tijd, 'temperatuur' => $row_temperatuur, 'grondvochtigheidslaag2' => $row_grondvochtigheidslaag1, 'grondvochtigheidslaag2' => $row_grondvochtigheidslaag1);
+		header('Content-type: text/javascript');
+		echo json_encode($data);
     }
     $result->free();
 }
