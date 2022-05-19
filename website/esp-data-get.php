d1a970b817391c2be514c928312c8b97bbf47195
PIETER JANSSEN <57659822+Pieter-janssen@users.noreply.github.com>
2022-05-19 14:54:04 +0200

    Update esp-data-get.php

--- website/esp-data-get.php
+++ website/esp-data-get.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT * FROM automatisch, control, pompoen1, pompoen2 where id = '1'";
+$sql = "SELECT * FROM automatisch, control, pompoen1, pompoen2";
 
 
  
@@ -31,7 +31,6 @@ if ($result = $conn->query($sql)) {
         $row_grondvochtigheidslaag2 = $row["grondvochtigheidlaag2"]; 
 		
 		//pompoen2
-        $row_tijd = $row["tijd"];
         $row_temperatuur2 = $row["temperatuur2"];
         $row_grondvochtigheidslaag12 = $row["grondvochtigheidlaag12"];
         $row_grondvochtigheidslaag22 = $row["grondvochtigheidlaag22"]; 
