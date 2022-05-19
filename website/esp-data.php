d8d03ce7322f6ca343179d0996c456115215ea1b
pieterjanssen <r0755997@student.thomasmore.be>
2022-05-19 13:27:15 +0200

    le

--- website/esp-data.php
+++ website/esp-data.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT * FROM pompoen1, automatisch WHERE id = '1'";
+$sql = "SELECT * FROM pompoen1 WHERE id = '1'";
 
 
  
@@ -36,31 +36,9 @@ if ($result = $conn->query($sql)) {
     $result->free();
 }
 
-$sql = "SELECT * FROM WHERE id = '1'";
 
 
  
-if ($result = $conn->query($sql)) {
-    while ($row = $result->fetch_assoc()) {
-        $row_ID = $row["ID"];
-        $row_ventilator1 = $row["ventilator1"];
-        $row_ventilator2 = $row["ventilator2"];
-        $row_raam1 = $row["raam1"];
-        $row_raam2 = $row["raam2"]; 
-		$row_deur1 = $row["deur1"]; 
-		$row_deur2 = $row["deur2"]; 
-		$row_vat1 = $row["vat1"];
-        $row_vat2 = $row["vat2"]; 
-		$row_vat3 = $row["vat3"]; 
-		$row_licht = $row["licht"]; 
-
-		
-		$data = array('ID' => $row_ID, 'ventilator1' => $row_ventilator1, 'ventilator2' => $row_ventilator2, 'raam1' => $row_raam1, 'raam2' => $row_raam2, 'deur1' => $row_deur1, 'deur2' => $row_deur2, 'vat1' => $row_vat1, 'vat2' => $row_vat2, 'vat3' => $row_vat3, 'licht' => $row_licht);
-		header('Content-type: text/javascript');
-		echo json_encode($data);
-    }
-    $result->free();
-}
 
 $conn->close();
 ?> 
