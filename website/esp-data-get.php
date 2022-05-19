4d7fce0ffcff16bc35143c4f449892283415ead1
PIETER JANSSEN <57659822+Pieter-janssen@users.noreply.github.com>
2022-05-19 14:31:34 +0200

    Update esp-data-get.php

--- website/esp-data-get.php
+++ website/esp-data-get.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT * FROM automatisch, control, pompoen1, pompoen2";
+$sql = "SELECT * FROM automatisch, control, pompoen1, pompoen2 where ID = '1'";
 
 
  
