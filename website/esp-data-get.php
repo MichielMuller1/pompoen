1182418c0375ab15ee6bac445b61a200c1e5d855
pieterjanssen <r0755997@student.thomasmore.be>
2022-05-19 13:23:59 +0200

    3

--- website/esp-data-get.php
+++ website/esp-data-get.php
@@ -27,23 +27,7 @@ if ($result = $conn->query($sql)) {
         $row_temperatuur = $row["temperatuur"];
         $row_grondvochtigheidslaag1 = $row["grondvochtigheidlaag1"];
         $row_grondvochtigheidslaag2 = $row["grondvochtigheidlaag2"]; 
-
-      
-		$data = array('tijd' => $row_tijd, 'temperatuur' => $row_temperatuur, 'grondvochtigheidslaag1' => $row_grondvochtigheidslaag1, 'grondvochtigheidslaag2' => $row_grondvochtigheidslaag1);
-		header('Content-type: text/javascript');
-		echo json_encode($data);
-    }
-    $result->free();
-}
-
-$sql = "SELECT * FROM WHERE id = '1'";
-
-
- 
-if ($result = $conn->query($sql)) {
-    while ($row = $result->fetch_assoc()) {
-        $row_ID = $row["ID"];
-        $row_ventilator1 = $row["ventilator1"];
+		$row_ventilator1 = $row["ventilator1"];
         $row_ventilator2 = $row["ventilator2"];
         $row_raam1 = $row["raam1"];
         $row_raam2 = $row["raam2"]; 
@@ -53,14 +37,18 @@ if ($result = $conn->query($sql)) {
         $row_vat2 = $row["vat2"]; 
 		$row_vat3 = $row["vat3"]; 
 		$row_licht = $row["licht"]; 
-
 		
-		$data = array('ID' => $row_ID, 'ventilator1' => $row_ventilator1, 'ventilator2' => $row_ventilator2, 'raam1' => $row_raam1, 'raam2' => $row_raam2, 'deur1' => $row_deur1, 'deur2' => $row_deur2, 'vat1' => $row_vat1, 'vat2' => $row_vat2, 'vat3' => $row_vat3, 'licht' => $row_licht);
+
+      
+		$data = array('tijd' => $row_tijd, 'temperatuur' => $row_temperatuur, 'grondvochtigheidslaag1' => $row_grondvochtigheidslaag1, 'grondvochtigheidslaag2' => $row_grondvochtigheidslaag1
+		 'ventilator1' => $row_ventilator1, 'ventilator2' => $row_ventilator2, 'raam1' => $row_raam1, 'raam2' => $row_raam2, 'deur1' => $row_deur1, 'deur2' => $row_deur2, 'vat1' => $row_vat1, 
+		 'vat2' => $row_vat2, 'vat3' => $row_vat3, 'licht' => $row_licht);
 		header('Content-type: text/javascript');
 		echo json_encode($data);
     }
     $result->free();
 }
+
 
 $conn->close();
 ?> 
