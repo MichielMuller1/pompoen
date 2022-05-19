a34bb61081916b7eff3b13d7c5e6658857d98f02
pieterjanssen <r0755997@student.thomasmore.be>
2022-05-19 14:40:39 +0200

    m

--- website/esp-data.php
+++ website/esp-data.php
@@ -17,28 +17,40 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT * FROM pompoen1 WHERE id = '1'";
+$sql = "SELECT * FROM pompoen1, pompoen2 where id = '1'";
 
 
  
 if ($result = $conn->query($sql)) {
     while ($row = $result->fetch_assoc()) {
+		
+		//pompoen1
         $row_tijd = $row["tijd"];
         $row_temperatuur = $row["temperatuur"];
         $row_grondvochtigheidslaag1 = $row["grondvochtigheidlaag1"];
         $row_grondvochtigheidslaag2 = $row["grondvochtigheidlaag2"]; 
+		
+		//pompoen2
+        $row_tijd = $row["tijd"];
+        $row_temperatuur2 = $row["temperatuur2"];
+        $row_grondvochtigheidslaag12 = $row["grondvochtigheidlaag12"];
+        $row_grondvochtigheidslaag22 = $row["grondvochtigheidlaag22"]; 
+		
+		
+
+		
 
       
-		$data = array('tijd' => $row_tijd, 'temperatuur' => $row_temperatuur, 'grondvochtigheidslaag1' => $row_grondvochtigheidslaag1, 'grondvochtigheidslaag2' => $row_grondvochtigheidslaag1);
+		$data = array('tijd' => $row_tijd,
+		'temperatuur' => $row_temperatuur, 'grondvochtigheidslaag1' => $row_grondvochtigheidslaag1, 'grondvochtigheidslaag2' => $row_grondvochtigheidslaag1,
+		'temperatuur2' => $row_temperatuur2, 'grondvochtigheidslaag12' => $row_grondvochtigheidslaag12, 'grondvochtigheidslaag22' => $row_grondvochtigheidslaag22
+		);
 		header('Content-type: text/javascript');
 		echo json_encode($data);
     }
     $result->free();
 }
 
-
-
- 
 
 $conn->close();
 ?> 
