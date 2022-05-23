21dbd287b7e1c645b8058609dda5da03835dab45
PIETER JANSSEN <57659822+Pieter-janssen@users.noreply.github.com>
2022-05-23 12:16:59 +0200

    Update esp-data-get.php

--- website/esp-data-get.php
+++ website/esp-data-get.php
@@ -37,52 +37,52 @@ if ($result = $conn->query($sql)) {
 		
 		
 		//control
-		$row_ventilator1 = $row["ventilator1"];
+	$row_ventilator1 = $row["ventilator1"];
         $row_ventilator2 = $row["ventilator2"];
         $row_raam1 = $row["raam1"];
         $row_raam2 = $row["raam2"]; 
-		$row_deur1 = $row["deur1"]; 
-		$row_deur2 = $row["deur2"]; 
-		$row_vat1bijvullen = $row["vat1bijvallen"];
-		$row_vat1wateren = $row["vat1wateren"];
-		$row_vat2bijvullen = $row["vat2bijvallen"];
-		$row_vat2wateren = $row["vat2wateren"];
-		$row_vat3bijvullen = $row["vat3bijvallen"];
-		$row_vat3wateren = $row["vat3wateren"];
-		$row_licht = $row["licht"]; 
+	$row_deur1 = $row["deur1"]; 
+	$row_deur2 = $row["deur2"]; 
+	$row_vat1bijvullen = $row["vat1bijvallen"];
+	$row_vat1wateren = $row["vat1wateren"];
+	$row_vat2bijvullen = $row["vat2bijvallen"];
+	$row_vat2wateren = $row["vat2wateren"];
+	$row_vat3bijvullen = $row["vat3bijvallen"];
+	$row_vat3wateren = $row["vat3wateren"];
+	$row_licht = $row["licht"]; 
 
 		
 		//automatisch
-		$row_ventilator1A = $row["ventilator1A"];
+	$row_ventilator1A = $row["ventilator1A"];
         $row_ventilator2A = $row["ventilator2A"];
         $row_raam1A = $row["raam1A"];
         $row_raam2A = $row["raam2A"]; 
-		$row_deur1A = $row["deur1A"]; 
-		$row_deur2A = $row["deur2A"]; 
-		$row_vat1A = $row["vat1A"];
+	$row_deur1A = $row["deur1A"]; 
+	$row_deur2A = $row["deur2A"]; 
+	$row_vat1A = $row["vat1A"];
         $row_vat2A = $row["vat2A"]; 
-		$row_vat3A = $row["vat3A"]; 
-		$row_lichtA = $row["lichtA"]; 
+	$row_vat3A = $row["vat3A"]; 
+	$row_lichtA = $row["lichtA"]; 
 	    
 	    //tresholds
-	    	$row_tempventilator1T = $row["tempventilator1T"];
-	    	$row_tempventilator2T = $row["tempventilator2T"];
-	    	$row_tempraam1T = $row["tempraam1T"];
-	    	$row_tempraam2T = $row["tempraam2T"];
-	    	$row_tempdeur1T = $row["tempdeur1T"];
-	    	$row_tempdeur2T = $row["tempdeur2T"];
-	    	$row_minvat1T = $row["minvat1T"];
-	    	$row_maxvat1T = $row["maxvat1T"];
-	    	$row_minvat2T = $row["minvat2T"];
-	    	$row_maxvat2T = $row["maxvat2T"];
-	    	$row_minvat3T = $row["minvat3T"];
-	    	$row_maxvat3T = $row["maxvat3T"];
-	    	$row_grondvochtigheid1laag1T = $row["grondvochtigheid1laag1T"];
-	    	$row_grondvochtigheid1laag2T = $row["grondvochtigheid1laag2T"];
-	    	$row_grondvochtigheid2laag12T = $row["grondvochtigheid2laag12T"];
-	    	$row_grondvochtigheid2laag22T = $row["grondvochtigheid2laag22T"];
-	    	$row_lichtT = $row["lichtT"];
-	    	$row_lichtkleurT = $row["lichtkleurT"];
+	 $row_tempventilator1T = $row["tempventilator1T"];
+	 $row_tempventilator2T = $row["tempventilator2T"];
+	 $row_tempraam1T = $row["tempraam1T"];
+	 $row_tempraam2T = $row["tempraam2T"];
+	 $row_tempdeur1T = $row["tempdeur1T"];
+	 $row_tempdeur2T = $row["tempdeur2T"];
+	 $row_minvat1T = $row["minvat1T"];
+	 $row_maxvat1T = $row["maxvat1T"];
+	 $row_minvat2T = $row["minvat2T"];
+	 $row_maxvat2T = $row["maxvat2T"];
+	 $row_minvat3T = $row["minvat3T"];
+	 $row_maxvat3T = $row["maxvat3T"];
+	 $row_grondvochtigheid1laag1T = $row["grondvochtigheid1laag1T"];
+	 $row_grondvochtigheid1laag2T = $row["grondvochtigheid1laag2T"];
+	 $row_grondvochtigheid2laag12T = $row["grondvochtigheid2laag12T"];
+	 $row_grondvochtigheid2laag22T = $row["grondvochtigheid2laag22T"];
+	 $row_lichtT = $row["lichtT"];
+	 $row_lichtkleurT = $row["lichtkleurT"];
 	    	
 		
 
@@ -98,11 +98,11 @@ if ($result = $conn->query($sql)) {
 		'vat1wateren' => $row_vat1wateren,
 		'vat2bijvullen' => $row_vat2bijvullen,
 		'vat2wateren' => $row_vat2wateren,
-		'vat2bijvullen' => $row_vat2bijvullen,
-		'vat2wateren' => $row_vat2wateren,
+		'vat3bijvullen' => $row_vat3bijvullen,
+		'vat3wateren' => $row_vat3wateren,
+		'licht' => $row_licht,
 		'ventilator1A' => $row_ventilator1A, 
 		'ventilator2A' => $row_ventilator2A, 
-		'licht' => $row_licht,
 		'raam1A' => $row_raam1A, 
 		'raam2A' => $row_raam2A, 
 		'deur1A' => $row_deur1A,
