588d841f0a40d3d4c58256242db254a69414244f
PIETER JANSSEN <57659822+Pieter-janssen@users.noreply.github.com>
2022-05-23 12:08:36 +0200

    Update esp-data.php

--- website/esp-data.php
+++ website/esp-data.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT * FROM pompoen1, pompoen2 where id = '1'";
+$sql = "SELECT * FROM pompoen1, pompoen2";
 
 
  
