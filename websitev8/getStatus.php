34de9298987c6594291bde99255ddd2bc20707d0
pieterjanssen <r0755997@student.thomasmore.be>
2022-05-27 23:14:55 +0200

    p

--- websitev8/getStatus.php
+++ websitev8/getStatus.php
@@ -17,11 +17,11 @@ try {
     $stmt->execute();
     $result = $stmt->fetchAll();
 
-    $stmt = $conn->prepare("select * from `pompoen1` order by ID desc limit 0,1");
+    $stmt = $conn->prepare("select * from `pompoen1`");
     $stmt->execute();
     $result2 = $stmt->fetchAll();
 
-    $stmt = $conn->prepare("select * from `pompoen2` order by ID desc limit 0,1");
+    $stmt = $conn->prepare("select * from `pompoen2`");
     $stmt->execute();
     $result3 = $stmt->fetchAll();
 
