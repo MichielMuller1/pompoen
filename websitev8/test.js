bb63224df7a2f2466dbafac5f4cba9299bc0cbf7
pieterjanssen <r0755997@student.thomasmore.be>
2022-05-29 20:19:13 +0200

    lel

--- websitev8/test.js
+++ websitev8/test.js
@@ -1,31 +1,15 @@
-const mariadb = require('mariadb');
-const pool = mariadb.createPool({
+var mysql = require('mysql');
+var con = mysql.createConnection({
     host: 'localhost',
     user: 'pi',
     password: 'raspberry',
     database: "pompoen"
 });
 
-pool.getConnection()
-    .then(conn => {
-
-        conn.query("SELECT 1 as val")
-            .then((rows) => {
-                console.log(rows); //[ {val: 1}, meta: ... ]
-                //Table must have been created before 
-                // " CREATE TABLE myTable (id int, val varchar(255)) "
-                return conn.query("UPDATE `water` SET `roerder` = '1' where id = '1'");
-            })
-            .then((res) => {
-                console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-                conn.end();
-            })
-            .catch(err => {
-                //handle error
-                console.log(err);
-                conn.end();
-            })
-
-    }).catch(err => {
-        //not connected
+con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT * FROM water", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
     });
+});
