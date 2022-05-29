c89807c6915c9a34119a5dde4ee577020b244db3
pieterjanssen <r0755997@student.thomasmore.be>
2022-05-29 20:41:48 +0200

    lel

--- websitev8/test.js
+++ websitev8/test.js
@@ -1,15 +1,36 @@
-var mysql = require('mysql');
-var con = mysql.createConnection({
+const mariadb = require('mariadb');
+const pool = mariadb.createConnection({
     host: 'localhost',
     user: 'pi',
     password: 'raspberry',
     database: "pompoen"
 });
 
-con.connect(function (err) {
-    if (err) throw err;
-    con.query("SELECT * FROM water", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
+pool.getConnection()
+    .then(conn => {
+
+        conn.query("SELECT 1 as val")
+            .then((rows) => {
+                console.log(rows); //[ {val: 1}, meta: ... ]
+                //Table must have been created before 
+                // " CREATE TABLE myTable (id int, val varchar(255)) "
+                return conn.query("SELECT * FROM water");
+            })
+            .then((res) => {
+                console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+                conn.end();
+            })
+            .catch(err => {
+                //handle error
+                console.log(err);
+                conn.end();
+            })
+
+    }).catch(err => {
+        //not connected
     });
-});
+
+
+
+
+
