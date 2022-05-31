2ee273b08fd15e16a2ef3f1b8235992e51d6756f
Michiel Muller <r0840937@student.thomasmore.be>
2022-05-31 13:02:51 +0200

    e

--- websitev8/esp-data-getv8.php
+++ websitev8/esp-data-getv8.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT * FROM automatisch, controls, pompoen1, pompoen2, threshold, water, weersvoorspelling";
+$sql = "SELECT * FROM automatisch, controls, pompoen1, pompoen2, threshold, water, weersvoorspelling, serre";
 
 
  
